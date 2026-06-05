298a1967122691ac039b8a79c78f978b56988781
tmatto <thi.matto@gmail.com>
2026-06-05 16:02:58 -0300

    tela de acesso

--- playwright/e2e/online.spec.ts
+++ playwright/e2e/online.spec.ts
@@ -1,16 +1,7 @@
 import { test, expect } from '@playwright/test';
 
-test('a webapp deve estar online', async ({ page }) => {
+test('webapp deve estar online', async ({ page }) => {
   await page.goto('http://localhost:5173');
-  await expect(page).toHaveTitle(/Velô by Papito/);
-});
+  await expect(page).toHaveTitle(/Velô by Papito/)
+})
 
-test('webapp deve ter o header', async ({ page }) => {
-  await page.goto('http://localhost:5173');
-  await expect(page.getByTestId('header')).toBeVisible();
-});
-
-test('webapp deve ter o footer', async ({ page }) => {
-  await page.goto('http://localhost:5173');
-  await expect(page.getByTestId('footer')).toBeVisible();
-});
