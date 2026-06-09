b9d3bff83def14a4811813b83f8d77b8c6edd043
tmatto <thi.matto@gmail.com>
2026-06-09 14:20:55 -0300

    padrao AAA

--- playwright/e2e/pedido.spec.ts
+++ playwright/e2e/pedido.spec.ts
@@ -1,12 +1,19 @@
-import { test, expect } from '@playwright/test'
+import { test, expect } from '@playwright/test';
+
+const ORDER_ID = 'VLO-6GTS89';
 
 test('deve consultar um pedido', async ({ page }) => {
-  await page.goto('http://localhost:5173/')
-  await page.getByRole('link', { name: 'Consultar Pedido' }).click()
-  await expect(page.getByRole('heading')).toContainText('Consultar Pedido')
-  await page.getByTestId('search-order-id').click()
-  await page.getByTestId('search-order-id').click()
-  await page.getByTestId('search-order-id').fill('VLO-6GTS89')
-  await page.getByTestId('search-order-button').click()
-  await expect(page.getByTestId('order-result-id')).toContainText('VLO-6GTS89')
-})
+  // Arrange
+  await page.goto('http://localhost:5173/');
+  await expect(page.getByTestId('hero-section').getByRole('heading')).toContainText('Velô Sprint');
+
+  // Act
+  await page.getByRole('link', { name: 'Consultar Pedido' }).click();
+  await page.getByTestId('search-order-id').fill(ORDER_ID);
+  await page.getByTestId('search-order-button').click();
+
+  // Assert
+  await expect(page.getByRole('heading')).toContainText('Consultar Pedido');
+  await expect(page.getByTestId('order-result-id')).toContainText(ORDER_ID);
+  await expect(page.getByTestId('order-result-status')).toContainText('APROVADO');
+});
