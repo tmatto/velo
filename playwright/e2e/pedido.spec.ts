c6f31dd7c105b96cb0355a6b14d9516d83011d2c
tmatto <thi.matto@gmail.com>
2026-07-01 09:48:18 -0300

    desafio dos seletores

--- playwright/e2e/pedido.spec.ts
+++ playwright/e2e/pedido.spec.ts
@@ -13,6 +13,11 @@ test('deve consultar um pedido', async ({ page }) => {
   await page.getByTestId('search-order-button').click()
 
   // Assert
-  await expect(page.getByText(ORDER_ID)).toBeVisible();
+  const containerPedido = page.getByRole('paragraph')
+    .filter({ hasText: /^Pedidos$/ })
+    .locator('..') // Sobe para o elemento pai (a div que agrupa ambos)
+
+await expect(containerPedido).toContainText(ORDER_ID)
+
   await expect(page.getByText('APROVADO')).toBeVisible();
 })
